0925fafa297b8f547118cd733b5304f1339a6a4b
lulamae12 <tls.tommy11@gmail.com>
2018-03-01 21:14:25 -0600

    added better recognition and added eye recognition

--- imageclassifier.py
+++ imageclassifier.py
@@ -3,16 +3,32 @@
 import numpy as np
 import sys
 import cv2
-import tensorflow as tf# just used ti test
-cascPath = 'haarcascade_frontalface.xml'
-faceCascade = cv2.CascadeClassifier(cascPath)
+import tensorflow as tf
+import tensorflow
+defaultcam = cv2.VideoCapture(2)
+
+facecascPath = 'haarcascade_frontalface.xml'#face
+faceCascade = cv2.CascadeClassifier(facecascPath)
+
+
+
+
+
+
+
+eyeindicascPath = 'cascades\haarcascade_eye.xml'#eyeindi
+eyeindiCascade = cv2.CascadeClassifier(eyeindicascPath)
+
+
+
+
 hello = tf.constant("hello world")
 sess = tf.Session()
 print(sess.run(hello))
 
 
 
-defaultcam = cv2.VideoCapture(2)
+
 print("if cam feed wont display make sure the VideoCapture is set to the correct num. realsense:2, 7559:0")
 while(True):
     ret, frame = defaultcam.read()# returns t/f if frame is read correctly
@@ -24,12 +40,32 @@ while(True):
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-    
+
 
 
     # Draw a rectangle around the face
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+
+
+
+    eyeindi = eyeindiCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+
+
+    # Draw a rectangle around the eye
+    for (x, y, w, h) in eyeindi:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
+
+
     cv2.imshow('Press "q" to exit', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
